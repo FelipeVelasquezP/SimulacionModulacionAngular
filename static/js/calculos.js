5d3957537c9b4e7c7fe62a4a2ea6e1f7346974d8
ACLXRD <andres.lopez01@correo.usa.edu.co>
2021-04-24 01:05:56 -0500

    Se agregan tarjetas para las ecuaciones

--- static/js/calculos.js
+++ static/js/calculos.js
@@ -1,5 +1,9 @@
-
 function getIndiceModulacion(Em,Ec) {
-var operacion=1;
-return operacion;    
+    var operacion=1;
+    return operacion;    
+}
+
+function getAnchoBanda(fm) {
+    var operacion = 2*(fm);
+    return operacion;    
 }
--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,18 @@
-//Caputra de datos
-console.log("hola")
 
 $("#btn-calcular").click(function() {
+  //Caputra de datos
   var Vc=parseFloat($("#Vc").val());
   var fc=parseFloat($("#fc").val());
   var Vm=parseFloat($("#Vm").val());
   var fm=parseFloat($("#fm").val());
+  //Validación
   if (Vc && fc && Vm && fm) {
     graficaModuladoraFM(Vm, fm);
     graficaModuladoraPM(Vm,fm);
     $(".inputs").hide();
+    $(".box-2").css("display","block")
+    $("#anchoBanda").text(getAnchoBanda(Vm)+" Hz")
+
   } else {
     Swal.fire({
         icon: 'error',
